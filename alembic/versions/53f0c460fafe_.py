53bf17a648b1669b563ad749e144a78cd00fc4ce
Anzor Khamukov <AnzorKhamukov@yandex.ru>
2023-12-23 02:22:08 +0300

    first commit

---
+++ alembic/versions/53f0c460fafe_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 53f0c460fafe
+Revises: 
+Create Date: 2023-12-23 01:53:05.894381
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '53f0c460fafe'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
